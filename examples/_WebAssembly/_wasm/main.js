53787e846493472128b632b3982f2ae56d676830
Sissi-2017 <542722462@qq.com>
2017-10-23 18:34:29 +0800

    Update main.js

--- examples/_WebAssembly/_wasm/main.js
+++ examples/_WebAssembly/_wasm/main.js
@@ -5,3 +5,23 @@ var worker = new Worker('js/wasm-worker.js');
 worker.postMessage({
   data: WemAssembly.Module(wasmCode);
 });
+
+
+//computeFake的功能：打印784个数？？？？？？？？？？？？
+
+var result = new ArrayBuffer(784);
+
+myWorker.onmessage = function(e) {
+	
+  for(i = 0; i < 28*28; ++i)
+  {
+     result.push(e.data);                         //data可否设为一个数组？？？？            
+     //console.log('Message received from worker');
+  }
+     
+}
+
+console.log('Number =', result);             //computeFake的返回形式？？？？？
+
+
+
