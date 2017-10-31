c6135f824904de596f0f2ed334d51a3d32481091
Sissi-2017 <542722462@qq.com>
2017-10-31 17:34:35 +0800

    Update loader.js

--- examples/_WebAssembly/_wasm/loader.js
+++ examples/_WebAssembly/_wasm/loader.js
@@ -15,12 +15,21 @@
 // }
 
 
-
-function loadWebAssembly (path) {
-  return fetch(path)                   // 加载文件        
-    .then(res => res.arrayBuffer())    // 转成 ArrayBuffer
-    .then(WebAssembly.instantiate)     // 编译 + 实例化
-    .then(mod => mod.instance)         // 提取生成都模块
+function loadWebAssembly(filename, imports = {}) {
+  return fetch(filename)
+    .then(response => response.arrayBuffer())
+    .then(bytes =>
+    WebAssembly.instantiate(bytes))
+    .then(module => {
+      imports.env = imports.env || {}
+      Object.assign(imports.env, {
+        memoryBase: 0,
+        tableBase: 0,
+        memory: new WebAssembly.Memory({ initial: 256, maximum: 256 }),
+        table: new WebAssembly.Table({ initial: 0, maximum: 0, element: 'anyfunc' })
+      })
+      return new WebAssembly.Instance(module, imports)
+    })
 }
 
 
