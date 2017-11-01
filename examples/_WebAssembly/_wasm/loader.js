ab194b20615785bad4612f928f17d080423b72e7
Sissi-2017 <542722462@qq.com>
2017-11-01 10:17:35 +0800

    Update loader.js

--- examples/_WebAssembly/_wasm/loader.js
+++ examples/_WebAssembly/_wasm/loader.js
@@ -1,19 +1,30 @@
-function loadWebAssembly(filename, imports = {}) {
-  return fetch(filename)
-    .then(response => response.arrayBuffer())
-    .then(buffer => WebAssembly.compile(buffer))
-    .then(module => {
-      imports.env = imports.env || {}
-      Object.assign(imports.env, {
-        memoryBase: 0,
-        tableBase: 0,
-        memory: new WebAssembly.Memory({ initial: 256, maximum: 256 }),
-        table: new WebAssembly.Table({ initial: 0, maximum: 0, element: 'anyfunc' })
-      })
-      return new WebAssembly.Instantiate(module,imports)
-    })
-}
+// function loadWebAssembly(filename, imports = {}) {
+//   return fetch(filename)
+//     .then(response => response.arrayBuffer())
+//     .then(buffer => WebAssembly.compile(buffer))
+//     .then(module => {
+//       imports.env = imports.env || {}
+//       Object.assign(imports.env, {
+//         memoryBase: 0,
+//         tableBase: 0,
+//         memory: new WebAssembly.Memory({ initial: 256, maximum: 256 }),
+//         table: new WebAssembly.Table({ initial: 0, maximum: 0, element: 'anyfunc' })
+//       })
+//       return new WebAssembly.Instance(module,imports)
+//     })
+// }
 
+
+
+function fetchAndInstantiate(url, importObject) {
+  return fetch(url).then(response =>
+    response.arrayBuffer()
+  ).then(bytes =>
+    WebAssembly.instantiate(bytes, importObject)
+  ).then(results =>
+    results.instance
+  );
+}
 
 
 
